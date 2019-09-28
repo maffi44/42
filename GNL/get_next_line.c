4163d56571a36c832b42e8bd32e91de19ea7ab9b
Meowth Camila <mcamila@ox-e4.21-school.ru>
2019-09-28 21:19:46 +0300

    gnl update

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mcamila <mcamila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 18:05:17 by mcamila           #+#    #+#             */
-/*   Updated: 2019/09/28 13:58:51 by mcamila          ###   ########.fr       */
+/*   Updated: 2019/09/28 21:11:47 by mcamila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -138,4 +138,66 @@ int				get_next_line(const int fd, char **line)
 	return (-1);
 }
 
+int				ft_seek_line(char *s, int rd, char **line)
+{
+	size_t	i;
 
+	i = 0;
+	while (s[i] != '\n' && s[i] != '\0')
+		i++;
+	if (s[i] == '\n' || (rd == 0 && ft_strlen(s) > 0))
+	{
+		if (!(*line = (char*)malloc(i + 1)))
+			return (-1);
+		*line = ft_strncpy(s, i);
+		(*line)[i] = '\0';
+		free(s);
+		return (1)
+	}
+	return (rd);
+}
+
+int				ft_read_to_buf(t_list *list, int fd, char **line)
+{
+	char	buf[BUFF_SIZE];
+	char 	*buf2;
+	int 	rd;
+
+	if (!(list = ft_seeklst(fd, head)))
+	{
+		list = (t_list*)malloc(sizeof(t_list));
+		list->content = ft_strnew(1);
+		list->content_size = fd;
+	}
+	while (1)
+	{
+		rd = read(fd, buf, BUFF_SIZE);
+		if (rd < 0)
+			return (-1);
+		if (!(buf2 = ft_strdup(list->content)))
+			return (-1);
+		list->content = ft_strjoin(buf2, buf);
+		free(buf2);
+		if (!(list->content))
+			return (-1);
+		if (ft_seek_line(list->content, rd, line) == 1)
+			return (1);
+		
+	}
+
+
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	static t_list	*head;
+	t_list			*list;
+
+	if (!head)
+		head = ft_lstnew("", 1);
+	if (!head)
+		return (-1);
+	ft_read_to_buf(list, fd, line);
+
+
+}
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -17,6 +17,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef static struct	s_s
+{
+	int					fd;
+	char				*s;
+	struct s_s			*next;
+	struct s_s			*prev;
+}						t_s;
+
 int	get_next_line(const int fd, char **line);
 
 #define BUFF_SIZE 64
