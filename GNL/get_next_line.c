b2b9dcf32a4ac771e348bd3fe874b5e0ca1c10b1
Meowth Camila <mcamila@vo-j3.21-school.ru>
2019-10-01 18:55:15 +0300

    gnl1oct19

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mcamila <mcamila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 18:05:17 by mcamila           #+#    #+#             */
-/*   Updated: 2019/10/01 17:21:30 by mcamila          ###   ########.fr       */
+/*   Updated: 2019/10/01 18:53:44 by mcamila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,19 @@ static t_list	*ft_get_list(int fd, t_list *head)
 			return (NULL);
 		list->content = NULL;
 		list->content_size = fd;
+		list->next = NULL;
 		ft_lstadd(&head, list);
 	}
 	return (list);
 }
-static int		ft_seek_line(char *s, int rd, char **line)
+static int		ft_seek_line(char *s, int rd, char **line, t_list *list)
 {
 	size_t	i;
 	char	*buf;
 
 	i = 0;
+	if (s[i] == '\n')
+		i++;
 	while (s[i] != '\n' && s[i] != '\0')
 		i++;
 	if (s[i] == '\n' || (rd == 0 && ft_strlen(s) > 0))
@@ -56,13 +59,14 @@ static int		ft_seek_line(char *s, int rd, char **line)
 			s = NULL;
 			return (1);
 		}
-		i++;
-		if (!(buf = ft_strnew(ft_strlen(s) - i)))
+		if (s[i] == '\n')
+			i++;
+		if (!(buf = ft_strdup(&(s[i]))))
 			return (-1);
-		buf = ft_strcpy(buf, &s[i]);
+//		buf = ft_strcpy(buf, &s[i]);
 		free(s);
-		s = ft_strdup(buf);
-		ft_memdel((void**)&buf);
+		list->content = buf;
+//		ft_putendl(list->content);
 		return (1);
 	}
 	if (rd == 0 && s[i] == '\0')
@@ -87,13 +91,10 @@ static int		ft_read_to_buf(t_list *head, int fd, char **line)
 	{
 		if ((rd = read(fd, buf, BUFF_SIZE)) == -1)
 			return (-1);
-//		ft_putnbr(fd);
+//		ft_putnbr(rd);
 //		if (list->content)
 //			ft_putendl(list->content);
-		if (rd == 0)
-			return (0);
 		buf[rd] = '\0';
-
 		if (list->content || rd > 0)
 		{
 //			ft_putendl("fafafa");
@@ -108,7 +109,7 @@ static int		ft_read_to_buf(t_list *head, int fd, char **line)
 				free(buf2);
 			if (!(list->content))
 				return (-1);
-			if ((rd = ft_seek_line(list->content, rd, line)) != 0)
+			if ((rd = ft_seek_line(list->content, rd, line, list)) != 0)
 			{
 				if (rd == 2)
 					return (0);
@@ -130,6 +131,7 @@ int				get_next_line(const int fd, char **line)
 			return (-1);
 		head->content = NULL;
 		head->content_size = fd;
+		head->next = NULL;
 	}
 	return (ft_read_to_buf(head, fd, line));
 }
@@ -137,9 +139,71 @@ int				get_next_line(const int fd, char **line)
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <assert.h>
 
 int	main()
 {
+	char 	*line;
+	int		out;
+	int		p[2];
+	int		fd;
+	int		gnl_ret;
+
+	out = dup(1);
+	pipe(p);
+
+	fd = 1;
+	dup2(p[1], fd);
+	write(fd, "abc\n\n", 5);
+	close(p[1]);
+	dup2(out, fd);
+
+	/* Read abc and new line */
+	gnl_ret = get_next_line(p[0], &line);
+	if (gnl_ret == 1)
+		ft_putendl("Y!");
+	else
+		ft_putendl("NO");
+	if (strcmp(line, "abc") == 0)
+		ft_putendl("Y!");
+	else
+		ft_putendl("NO");
+
+	/* Read new line */
+	gnl_ret = get_next_line(p[0], &line);
+
+	if (gnl_ret == 1)
+		ft_putendl("Y!");
+	else
+		ft_putendl("NO");
+	if (line == NULL || *line == '\0')
+		ft_putendl("Y!");
+	else
+		ft_putendl("NO");
+
+	/* Read again, but meet EOF */
+	gnl_ret = get_next_line(p[0], &line);
+	if (gnl_ret == 0)
+		ft_putendl("Y!");
+	else
+		ft_putendl("NO");
+	if (line == NULL || *line == '\0')
+		ft_putendl("Y!");
+	else
+		ft_putendl("NO");
+
+	/* Let's do it once again */
+	gnl_ret = get_next_line(p[0], &line);
+	if (gnl_ret == 0)
+		ft_putendl("Y!");
+	else
+		ft_putendl("NO");
+	if (line == NULL || *line == '\0')
+		ft_putendl("Y!");
+	else
+		ft_putendl("NO");
+	return (0);
+ /*
 	int fd;
 	int rd;
 	char *line;
@@ -157,5 +221,5 @@ int	main()
 //		ft_putchar('0');
 		free(line);
 	}
-	return (0);
+	return (0);*/
 }
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mcamila <mcamila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 19:31:12 by mcamila           #+#    #+#             */
-/*   Updated: 2019/09/28 13:44:13 by mcamila          ###   ########.fr       */
+/*   Updated: 2019/10/01 18:17:52 by mcamila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@
 
 int	get_next_line(const int fd, char **line);
 
-#define BUFF_SIZE 100000
+#define BUFF_SIZE 777
 
 #endif
