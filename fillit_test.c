e5bd795b0a0f6c52e19f2a839690f4dea2752b68
Meowth Camila <mcamila@il-f1.21-school.ru>
2019-11-05 20:26:49 +0300

    fillit ft_Y ready

--- fillit_test.c
+++ fillit_test.c
@@ -6,127 +6,86 @@
 /*   By: mcamila <mcamila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 17:29:19 by mcamila           #+#    #+#             */
-/*   Updated: 2019/11/05 14:39:57 by mcamila          ###   ########.fr       */
+/*   Updated: 2019/11/05 20:19:36 by mcamila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fillit.h"
-
-typedef struct s_fill
+typedef struct 	s_Tet
 {
-	char order;
-	int x_len;
-	int y_len;
-	char yx[4][4];
-}            t_fill;
+	short int	rows[121][4];
+	short int	length;
+}				t_Tet;
 
-typedef struct		s_node
+t_Tet			tets[26];
+short int		cols[144][26][4];
+unsigned char	masks[26][26][121];
+/*
+ * i - index of current tetramino
+ * j - index of column
+ * imax - number of tatraminos
+ * k - index of column elements
+ * s - indexes of the next tetraminos
+ */
+void			ft_disable_rows(int j, int i, int imax, unsigned char b)
 {
-	int x;
-}					t_node;
+	int k;
+	int s;
 
-typedef struct		s_col
-{
-	struct s_col	*left;
-	struct s_col	*right;
-	t_node			*head;
-	int				length;
-	int				name;
-}					t_col;
-
-void	ft_apocalipse(void);
-
-t_col	**ft_create_cols(int num)
-{
-	int		i;
-	t_col	**cols;
-
-	if (!(cols = (t_col**)malloc(sizeof((t_col*) * num))))
-		ft_apocalipse();
-	i = 0;
-	while (i < num)
+	s = i;
+	while (++s < imax)
 	{
-		if (!(cols[i] = (t_col*)malloc(sizeof(t_col))))
-			ft_apocalipse();
-		cols[i]->head = NULL;
-		cols[i]->length = 0;
-		cols[i]->name = i + 1;
-		if (i != 0)
+		k = 0;
+		while (k < 4)
 		{
-			cols[i - 1]->right = cols[i];
-			cols[i]->left = cols[i - 1];
+			masks[i][s][cols[j][s][k]] = b;
+			k++;
 		}
-		i++;
 	}
-	cols[num - 1]->right = cols[0];
-	cols[0]->left = cols[num - 1];
-	return cols;
 }
-
-int		ft_make_map(t_fill *figure, int tn)
+/*
+ * k - index of node
+ */
+void			ft_create_mask(int i, int j, int imax, unsigned char b)
 {
-	int ss; //length of square side
-	int i;
-	t_col	**cols;
+	int k;
 
-
-	ss = 2;
-	while (tn * 4 < ss * ss)
-		ss++;
-	i = 0;
-
-	cols = ft_create_cols(ss);
-	while (i < tn)
+	while (k < 4)
 	{
-		figure[i]
-		i++;
+		ft_disable_rows(tets[i].rows[j][k], i, imax, b);
+		k++;
 	}
 }
-
-int					main(int argc, char **argv)
+/*
+ * imax - number of tetrominos
+ * i - index of current tetramino
+ * j - index of a row (variants of tetromino's location) in current tetromino
+ */
+int		ft_Y(int imax, int i)
 {
-	t_fill				figure[26];
-	unsigned long int	*rows;
-	int					tn;
-	int					fd;
-	char				buf[BUFF_SIZE];
-
-	fd = open(av[1], O_RDONLY);
-	if (fd < 0 || ac != 2)
-		exit(0);
-	if ((tn = validity(fd, buf, figure) == 0)
+	short int	j;
+	if (i == imax)
 	{
-		//error
+		return (1);
 	}
-	rows = ft_make_map(figure, tn);
-
+	j = 0;
+	while (j < tets[i].length)
+	{
+		if (!(masks[i][i][j]))
+		{
+			ft_create_mask(i, j, imax, 1);
+			if (ft_Y(im, i + 1))
+				return (1);
+			ft_create_mask(i, j, imax, 0);
+		}
+		j++;
+	}
+	return (0);
 }
 
-typedef struct		s_Node
+void	ft_solve(int imax, int i, int map_side)
 {
-	struct s_Node	*right;
-	struct s_Node	*down;
-}					t_Node;
-
-typedef struct	s_C_Header
-{
-	t_Node		*first;
-}				t_C_Header;
-
-typedef struct	s_R_Header
-{
-	t_Node		node[4];
-}				t_R_Header;
-
-typedef struct	s_T_Header
-{
-	t_R_Header	rows[121];
-}				t_T_Header;
-
-t_T_Header		tetrHeaders[26];
-t_C_Header		ColHeaders[144];
-
-void	ft_routineY(int i)
-{
-
+	ft_make_map(map_side);
+	while (!(ft_Y(imax, i)))
+		ft_make_map(map_side + 1);
+	ft_draw_map();
 }
