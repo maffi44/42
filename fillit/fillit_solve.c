2bacc5d8fcd9a6cc6733f5e2c641aa09144e10b6
Meowth Camila <mcamila@il-e5.21-school.ru>
2019-11-07 14:48:29 +0300

    fillit 4

--- fillit/fillit_solve.c
+++ fillit/fillit_solve.c
@@ -6,19 +6,19 @@
 /*   By: mcamila <mcamila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 17:29:19 by mcamila           #+#    #+#             */
-/*   Updated: 2019/11/05 20:19:36 by mcamila          ###   ########.fr       */
+/*   Updated: 2019/11/07 14:28:49 by mcamila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 typedef struct 	s_Tet
 {
-	short int	rows[121][4];
-	short int	length;
+	int			rows[121][4];
+	int			length;
 }				t_Tet;
 
 t_Tet			tets[26];
-short int		cols[144][26][4];
-unsigned char	masks[26][26][121];
+int				cols[144][26][4];
+unsigned long	masks[26][26][2];
 /*
  * i - index of current tetramino
  * j - index of column
@@ -26,34 +26,42 @@ unsigned char	masks[26][26][121];
  * k - index of column elements
  * s - indexes of the next tetraminos
  */
-void			ft_disable_rows(int j, int i, int imax, unsigned char b)
+void			ft_disable_rows(int j, int i, int imax)
 {
 	int k;
 	int s;
 
 	s = i;
-	while (++s < imax)
+	while (s < imax)
 	{
+		masks[i][s][0] = masks[i - 1][s][0];
+		masks[i][s][1] = masks[i - 1][s][1];
 		k = 0;
 		while (k < 4)
 		{
-			masks[i][s][cols[j][s][k]] = b;
+			masks[i][s][cols[j][s][k] / 64] |= (unsigned long)1 << cols[j][s][k];
 			k++;
 		}
+		s++;
 	}
 }
 /*
  * k - index of node
  */
-void			ft_create_mask(int i, int j, int imax, unsigned char b)
+void			ft_create_mask(int i, int j, int imax)
 {
 	int k;
 
 	while (k < 4)
 	{
-		ft_disable_rows(tets[i].rows[j][k], i, imax, b);
+		ft_disable_rows(tets[i].rows[j][k], i + 1, imax);
 		k++;
 	}
+}
+
+int		ft_find_row()
+{
+	return (0);
 }
 /*
  * imax - number of tetrominos
@@ -64,18 +72,15 @@ int		ft_Y(int imax, int i)
 {
 	short int	j;
 	if (i == imax)
-	{
 		return (1);
-	}
 	j = 0;
 	while (j < tets[i].length)
 	{
-		if (!(masks[i][i][j]))
+		if ((masks[i][i][j / 64] >> j) & (unsigned long)1)
 		{
 			ft_create_mask(i, j, imax, 1);
-			if (ft_Y(im, i + 1))
+			if (ft_Y(imax, i + 1))
 				return (1);
-			ft_create_mask(i, j, imax, 0);
 		}
 		j++;
 	}
