b453e34453e883fe7d2554644318a080bba3f17d
mcamila <maffi44maffi44@gmail.com>
2020-02-19 12:04:33 +0300

    fdf norm midwork1

--- FDF/map_parser.c
+++ FDF/map_parser.c
@@ -6,64 +6,51 @@
 /*   By: mcamila <mcamila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 12:18:34 by mcamila           #+#    #+#             */
-/*   Updated: 2020/02/18 19:04:02 by mcamila          ###   ########.fr       */
+/*   Updated: 2020/02/19 11:59:44 by mcamila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 #include <fcntl.h>
 
-void	del_maps(t_maps *maps)
+void		del_maps(t_maps *maps)
 {
-	size_t i;
-	size_t j;
+	int	i;
+	int	j;
+
 	if (maps->temp)
 		ft_memdel((void*)&(maps->temp));
 	if (maps->big_line)
 		ft_memdel((void*)&(maps->big_line));
 	if (maps->map)
 	{
-		i = 0;
-		while ((maps->map)[i])
-		{
-			ft_memdel(((void *) &(maps->map)[i]));
-			i++;
-		}
+		i = -1;
+		while ((maps->map)[++i])
+			ft_memdel(((void*)&(maps->map)[i]));
 		ft_memdel((void*)&(maps->map));
 	}
 	if (maps->line)
 	{
-		i = 0;
-		while ((maps->line)[i])
-		{
-			ft_memdel((void *) &(maps->line)[i]);
-			i++;
-		}
+		i = -1;
+		while ((maps->line)[++i])
+			ft_memdel((void*)&(maps->line)[i]);
 		ft_memdel((void*)&(maps->line));
 	}
-
 	if (maps->big_map)
 	{
-		i = 0;
-		while ((maps->big_map)[i])
+		i = -1;
+		while ((maps->big_map)[++i])
 		{
-			j = 0;
-			while ((maps->big_map)[i][j])
-			{
-				ft_putendl("123");
-				ft_memdel(((void *) &((maps->big_map)[i][j])));
-				j++;
-			}
-			ft_putendl("12345");
+			j = -1;
+			while ((maps->big_map)[i][++j])
+				ft_memdel(((void*)&((maps->big_map)[i][j])));
 			ft_memdel(((void*)&(maps->big_map)[i]));
-			i++;
 		}
 		ft_memdel(((void*)&(maps->big_map)));
 	}
-
 }
 
-void	error(int error, t_data *data, t_maps *maps)
+void		error(int error, t_data *data, t_maps *maps)
 {
 	if (error == 0)
 		ft_putendl("usage: ./fdf [filename]");
@@ -76,103 +63,94 @@ void	error(int error, t_data *data, t_maps *maps)
 	go_away(data);
 }
 
-t_ref_obj	make_ref_obj_from_map(int **map, int x, int y, t_data *data)
+void		*super_malloc(t_data *data, t_maps *maps, size_t size)
 {
-	t_ref_obj obj;
-	int i;
-	int j;
-	int k;
-	t_color col;
+	void *memory;
+
+	if (!(memory = malloc(size)))
+		error(1, data, maps);
+	return (memory);
+}
+
+t_ref_obj	make_ref_obj_from_map(int x, int y, t_data *data, t_maps *maps)
+{
+	t_ref_obj	obj;
+	t_color		col;
+	int			i;
+	int			j;
+	int			k;
 
 	obj.num_of_pts = x * y;
 	obj.num_of_tris = (x - 1) * (y - 1) * 2;
-	if (!(obj.vertex = (t_vec3*)malloc(sizeof(t_vec3) * obj.num_of_pts)))
-		error(1, data, NULL);
-	if (!(obj.tri = (t_tri*)malloc(sizeof(t_tri) * obj.num_of_tris)))
-		error(1, data, NULL);
-
-	j = 0;
+	obj.vertex = (t_vec3*)super_malloc(data, maps, sizeof(t_vec3) * obj.num_of_pts);
+	obj.tri = (t_tri*)super_malloc(data, maps, sizeof(t_tri) * obj.num_of_tris);
 	k = 0;
 	col.ARGB = 0;
-	while (j < y)
+	j = -1;
+	while (++j < y)
 	{
-		i = 0;
-		while (i < x)
+		i = -1;
+		while (++i < x)
 		{
-			//if (map[j][i] < 5)
-			//	col.ARGB = 0x0000FFFF;
-			//else
-			//	col.ARGB = 0x00FFFF00;
-			col.colors[0] = map[j][i] * 4;
-			col.colors[1] = map[j][i] * 1;
-			col.colors[2] = map[j][i] * 13;
-			obj.vertex[k] = make_vertex((float)(i - x / 2), ((float)map[j][i]) / 5, (float)(j - y / 2), col.ARGB);
-			i++;
+			col.colors[0] = maps->map[j][i] * 4;
+			col.colors[1] = maps->map[j][i] * 1;
+			col.colors[2] = maps->map[j][i] * 13;
+			obj.vertex[k] = make_vertex((float)(i - x / 2), ((float)maps->map[j][i]) / 5, (float)(j - y / 2), col.ARGB);
 			k++;
 		}
-		j++;
 	}
-	j = 0;
 	k = 0;
-	while (j < y - 1)
+	j = -1;
+	while (++j < y - 1)
 	{
-		i = 0;
-		while (i < x - 1)
+		i = -1;
+		while (++i < x - 1)
 		{
-			if (abs(map[j][i] - map[j + 1][i + 1]) >= abs(map[j + 1][i] - map[j][i + 1]))
+			if (abs(maps->map[j][i] - maps->map[j + 1][i + 1]) >= abs(maps->map[j + 1][i] - maps->map[j][i + 1]))
 			{
 				obj.tri[k].pt[0] = (j * x) + i + 1;
 				obj.tri[k].pt[1] = (j * x) + i;
 				obj.tri[k].pt[2] = ((j + 1) * x) + i;
-
 				obj.tri[k + 1].pt[0] = ((j + 1) * x) + i + 1;
 				obj.tri[k + 1].pt[1] = (j * x) + i + 1;
 				obj.tri[k + 1].pt[2] = ((j + 1) * x) + i;
-
 			}
 			else
 			{
 				obj.tri[k].pt[0] = (j * x) + i + 1;
 				obj.tri[k].pt[1] = (j * x) + i;
 				obj.tri[k].pt[2] = ((j + 1) * x) + i + 1;
-
 				obj.tri[k + 1].pt[0] = ((j + 1) * x) + i + 1;
 				obj.tri[k + 1].pt[1] = (j * x) + i;
 				obj.tri[k + 1].pt[2] = ((j + 1) * x) + i;
 			}
-			i++;
 			k += 2;
 		}
-		j++;
 	}
+	del_maps(maps);
 	return (obj);
 }
 
-
-
 t_ref_obj	map_parser(char *file_name, t_data *data)
 {
-	int fd;
-	int y;
-	int x;
-	t_maps maps;
+	int		fd;
+	int		y;
+	int		x;
+	int		rd;
+	t_maps	maps;
 
 	maps.big_line = NULL;
 	maps.big_map = NULL;
 	maps.line = NULL;
 	maps.map = NULL;
 	maps.temp = NULL;
-
 	if ((fd = open(file_name, O_RDONLY)) < 0)
 		error(0, data, &maps);
-	if (!((maps.line) = (char**)malloc(sizeof(char*))))
-		error(1, data, &maps);
-	if (!((maps.big_line) = (char*)malloc(sizeof(char))))
-		error(1, data, &maps);
+	(maps.line) = (char**)super_malloc(data, &maps, sizeof(char*));
+	(maps.big_line) = (char*)super_malloc(data, &maps, sizeof(char));
 	*(maps.big_line) = '\0';
 	*(maps.line) = NULL;
 	y = 0;
-	int rd = -1;
 	while ((rd = get_next_line(fd, (maps.line))) > 0)
 	{
 		if (ft_strlen(*(maps.line)) == 0)
@@ -189,25 +167,17 @@ t_ref_obj	map_parser(char *file_name, t_data *data)
 		ft_memdel((void*)&*(maps.line));
 		y++;
 	}
-	printf("-->%d\n", rd);
-	if ( rd < 0 || ft_strlen((maps.big_line)) == 0)
-		error(0, data, &maps);
+	if (rd < 0 || ft_strlen((maps.big_line)) == 0 || y <= 1)
+		error(y, data, &maps);
 	ft_memdel((void*)&(maps.line));
 	if (!((maps.line) = ft_strsplit((maps.big_line), '/')))
 		error(1, data, &maps);
 	ft_memdel((void*)&(maps.big_line));
-	int e = 0;
-	y = 0;
-	while ((maps.line)[y])
-		y++;
-	if (y <= 1)
-		e = 1;
-	if (!((maps.big_map) = (char***)malloc(sizeof(char**) * (y + 1))))
-		error(1, data, &maps);
+	(maps.big_map) = (char***)super_malloc(data, &maps, sizeof(char**) * (y + 1));
 	(maps.big_map)[y] = NULL;
 	int i = 0;
 	int k;
-	int s = 0;
+	x = 0;
 	int b = 0;
 	while (i < y)
 	{
@@ -216,54 +186,25 @@ t_ref_obj	map_parser(char *file_name, t_data *data)
 		k = 0;
 		while ((maps.big_map)[i][k])
 			k++;
-		ft_memdel((void*)&(maps.line)[i]);
-		if ((s != k && b) || k == 1)
-			e = 1;
-		s = k;
+		if ((x != k && b) || k == 1)
+			error(2, data, &maps);
+		x = k;
 		b = 1;
 		i++;
 	}
-	ft_memdel((void*)&(maps.line));
-	x = 0;
-	while ((maps.big_map)[0][x])
-		x++;
-
-	if (!((maps.map) = (int**)malloc(sizeof(int*) * (y + 1))))
-		error(1, data, &maps);
+	(maps.map) = (int**)super_malloc(data, &maps, sizeof(int*) * (y + 1));
 	(maps.map)[y] = NULL;
-
-	i = 0;
-	while (i < y)
+	x = -1;
+	while ((maps.big_map)[0][++x]);
+	i = -1;
+	while (++i < y)
+	(maps.map)[i] = (int*)super_malloc(data, &maps, sizeof(int) * x);
+	i = -1;
+	while (++i < y)
 	{
-		if (!((maps.map)[i] = (int*)malloc(sizeof(int) * x)))
-			error(1, data, &maps);
-		i++;
-	}
-	i = 0;
-	while (i < y)
-	{
-		x = 0;
-		while ((maps.big_map)[i][x])
-		{
+		x = -1;
+		while ((maps.big_map)[i][++x])
 			(maps.map)[i][x] = ft_atoi((maps.big_map)[i][x]);
-			ft_memdel((void*)&(maps.big_map)[i][x]);
-			x++;
-		}
-		ft_memdel((void*)&(maps.big_map)[i]);
-		i++;
 	}
-	ft_memdel((void*)&(maps.big_map));
-
-	t_ref_obj obj = make_ref_obj_from_map((maps.map), x, y, data);
-
-	i = 0;
-	while (i < y)
-	{
-		ft_memdel((void*)&(maps.map)[i]);
-		i++;
-	}
-	ft_memdel((void*)&(maps.map));
-	if (e)
-		error(2, data, &maps);
-	return (obj);
+	return (make_ref_obj_from_map(x, y, data, &maps));
 }
