18166f3529c251a2580806d350b3f0558e80f17a
mcamila <maffi44maffi44@gmail.com>
2020-02-20 15:30:11 +0300

    fdf norm midmidwork

--- FDF/render.c
+++ FDF/render.c
@@ -6,7 +6,7 @@
 /*   By: mcamila <mcamila@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 17:55:37 by mcamila           #+#    #+#             */
-/*   Updated: 2020/02/20 11:40:16 by mcamila          ###   ########.fr       */
+/*   Updated: 2020/02/20 15:26:08 by mcamila          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ typedef struct		s_screen_tri
 	t_pt2	pt1;
 	t_pt2	pt2;
 	t_pt2	pt3;
-}					t_screen_tri;
+}					t_sc_tri;
 
 typedef struct		s_space_tri
 {
+	char 	bool;
 	t_vec3	vertex1;
 	t_vec3	vertex2;
 	t_vec3	vertex3;
@@ -28,9 +29,13 @@ typedef struct		s_space_tri
 	t_vec3	light_vector1;
 	t_vec3	light_vector2;
 	t_vec3	light_vector3;
-}					t_space_tri;
+}					t_sp_tri;
 
-t_vec3	seek_tri_center(t_vec3 vertex1, t_vec3 vertex2, t_vec3 vertex3)
+t_sc_tri	make_screen_tri(t_sp_tri sp, t_data *data, t_inst_obj obj, t_tri tri);
+t_vec3			seek_tri_center(t_vec3 vertex1, t_vec3 vertex2, t_vec3 vertex3);
+t_sp_tri		make_tri_in_space(t_inst_obj obj, t_tri tri, float d);
+
+inline t_vec3	seek_tri_center(t_vec3 vertex1, t_vec3 vertex2, t_vec3 vertex3)
 {
 	t_vec3 tri_center;
 
@@ -41,129 +46,71 @@ t_vec3	seek_tri_center(t_vec3 vertex1, t_vec3 vertex2, t_vec3 vertex3)
 	return (tri_center);
 }
 
-t_space_tri		*make_tri_in_space(t_space_tri *tr, t_inst_obj obj, t_tri tri, float d)
+inline t_sp_tri		make_tri_in_space(t_inst_obj obj, t_tri tri, float d)
 {
+	t_sp_tri tr;
 	t_vec3	edge1;
 	t_vec3	edge2;
 	t_vec3	tri_center;
 
-	if (((tr->vertex1 = vec3_transform(
+
+	tr.bool = 1;
+	if (((tr.vertex1 = vec3_transform(
 			obj.transform,
 			obj.ref_obj->vertex[tri.pt[0]]))
-				 .elem[2] / tr->vertex1.elem[3]) < d)
-		return (NULL);
-	if (((tr->vertex2 = vec3_transform(
+				 .elem[2] / tr.vertex1.elem[3]) < d)
+		return (tr);
+	if (((tr.vertex2 = vec3_transform(
 			obj.transform,
 			obj.ref_obj->vertex[tri.pt[1]]))
-				 .elem[2] / tr->vertex2.elem[3]) < d)
-		return (NULL);
-	if (((tr->vertex3 = vec3_transform(
+				 .elem[2] / tr.vertex2.elem[3]) < d)
+		return (tr);
+	if (((tr.vertex3 = vec3_transform(
 			obj.transform,
 			obj.ref_obj->vertex[tri.pt[2]]))
-				 .elem[2] / tr->vertex3.elem[3]) < d)
-		return (NULL);
-	edge1 = vec_divide(tr->vertex1, tr->vertex2);
-	edge2 = vec_divide(tr->vertex1, tr->vertex3);
-	tri_center = seek_tri_center(tr->vertex1, tr->vertex2, tr->vertex3);
-	tr->normal = vec_mult(edge1, edge2);
-	if (vec_scalar_mult(tr->normal, tri_center) <= 0)
-		return (NULL);
+				 .elem[2] / tr.vertex3.elem[3]) < d)
+		return (tr);
+	edge1 = vec_divide(tr.vertex1, tr.vertex2);
+	edge2 = vec_divide(tr.vertex1, tr.vertex3);
+	tri_center = seek_tri_center(tr.vertex1, tr.vertex2, tr.vertex3);
+	tr.normal = vec_mult(edge1, edge2);
+	if (vec_scalar_mult(tr.normal, tri_center) <= 0)
+		return (tr);
+	tr.bool = 0;
 	return (tr);
 }
 
-t_screen_tri	make_screen_tri(t_space_tri sp, t_data *data, t_inst_obj obj, t_tri tri)
+inline t_sc_tri	make_screen_tri(t_sp_tri sp, t_data *data, t_inst_obj obj, t_tri tri)
 {
-	t_screen_tri tr;
-
-	tr.pt1 = make_pt2_from_v3(sp.vertex1, data->d);
-	tr.pt2 = make_pt2_from_v3(sp.vertex2, data->d);
-	tr.pt3 = make_pt2_from_v3(sp.vertex3, data->d);
+	t_sc_tri sc;
 
 	sp.normal = normalize_vec(sp.normal);
 	sp.light_vector1 = vec_divide(data->dir_light, sp.vertex1);
 	sp.light_vector1 = normalize_vec(sp.light_vector1);
-
 	sp.light_vector2 = vec_divide(data->dir_light, sp.vertex2);
 	sp.light_vector2 = normalize_vec(sp.light_vector2);
-
 	sp.light_vector3 = vec_divide(data->dir_light, sp.vertex3);
 	sp.light_vector3 = normalize_vec(sp.light_vector3);
-
-	tr.pt1.light = vec_scalar_mult(sp.normal, sp.light_vector1) * 0.95f;
-	tr.pt2.light = vec_scalar_mult(sp.normal, sp.light_vector2) * 0.95f;
-	tr.pt3.light = vec_scalar_mult(sp.normal, sp.light_vector3) * 0.95f;
-
-	tr.pt1.color.ARGB = obj.ref_obj->vertex[tri.pt[0]].color;
-	tr.pt2.color.ARGB = obj.ref_obj->vertex[tri.pt[1]].color;
-	tr.pt3.color.ARGB = obj.ref_obj->vertex[tri.pt[2]].color;
-	return (tr);
+	sc.pt1 = make_pt2_from_v3(sp.vertex1, data->d);
+	sc.pt2 = make_pt2_from_v3(sp.vertex2, data->d);
+	sc.pt3 = make_pt2_from_v3(sp.vertex3, data->d);
+	sc.pt1.light = vec_scalar_mult(sp.normal, sp.light_vector1) * 0.95f;
+	sc.pt2.light = vec_scalar_mult(sp.normal, sp.light_vector2) * 0.95f;
+	sc.pt3.light = vec_scalar_mult(sp.normal, sp.light_vector3) * 0.95f;
+	sc.pt1.color.ARGB = obj.ref_obj->vertex[tri.pt[0]].color;
+	sc.pt2.color.ARGB = obj.ref_obj->vertex[tri.pt[1]].color;
+	sc.pt3.color.ARGB = obj.ref_obj->vertex[tri.pt[2]].color;
+	return (sc);
 }
 
 void	draw_triangle(t_inst_obj obj, t_tri tri, t_data *data, float d)
 {
-	t_space_tri space_tri;
-	t_screen_tri	screen_tri;
-	t_vec3	vertex1;
-	t_vec3	vertex2;
-	t_vec3	vertex3;
-	t_vec3	normal;
-	t_vec3	tri_center;
-	t_vec3	light_vector1;
-	t_vec3	light_vector2;
-	t_vec3	light_vector3;
-	t_vec3	edge1;
-	t_vec3	edge2;
-	t_pt2	pt1;
-	t_pt2	pt2;
-	t_pt2	pt3;
+	t_sp_tri		space_tri;
+	t_sc_tri	screen_tri;
 
-	if (!(make_tri_in_space(&space_tri, obj, tri, d)))
+	if ((space_tri = make_tri_in_space(obj, tri, d)).bool)
 		return ;
 	screen_tri = make_screen_tri(space_tri, data, obj, tri);
-//	if (((vertex1 = vec3_transform(
-//			obj.transform,
-//			obj.ref_obj->vertex[tri.pt[0]]))
-//				 .elem[2] / vertex1.elem[3]) < d)
-//		return ;
-//	if (((vertex2 = vec3_transform(
-//			obj.transform,
-//			obj.ref_obj->vertex[tri.pt[1]]))
-//				 .elem[2] / vertex2.elem[3]) < d)
-//		return ;
-//	if (((vertex3 = vec3_transform(
-//			obj.transform,
-//			obj.ref_obj->vertex[tri.pt[2]]))
-//				 .elem[2] / vertex3.elem[3]) < d)
-//		return ;
-//	edge1 = vec_divide(vertex1, vertex2);
-//	edge2 = vec_divide(vertex1, vertex3);
-//	tri_center = seek_tri_center(vertex1, vertex2, vertex3);
-//	normal = vec_mult(edge1, edge2);
-//	if (vec_scalar_mult(normal, tri_center) <= 0)
-//		return ;
-
-//	pt1 = make_pt2_from_v3(vertex1, d);
-//	pt2 = make_pt2_from_v3(vertex2, d);
-//	pt3 = make_pt2_from_v3(vertex3, d);
-//
-//	normal = normalize_vec(normal);
-//	light_vector1 = vec_divide(data->dir_light, vertex1);
-//	light_vector1 = normalize_vec(light_vector1);
-//
-//	light_vector2 = vec_divide(data->dir_light, vertex2);
-//	light_vector2 = normalize_vec(light_vector2);
-//
-//	light_vector3 = vec_divide(data->dir_light, vertex3);
-//	light_vector3 = normalize_vec(light_vector3);
-//
-//	pt1.light = vec_scalar_mult(normal, light_vector1) * 0.95f;
-//	pt2.light = vec_scalar_mult(normal, light_vector2) * 0.95f;
-//	pt3.light = vec_scalar_mult(normal, light_vector3) * 0.95f;
-//
-//	pt1.color.ARGB = obj.ref_obj->vertex[tri.pt[0]].color;
-//	pt2.color.ARGB = obj.ref_obj->vertex[tri.pt[1]].color;
-//	pt3.color.ARGB = obj.ref_obj->vertex[tri.pt[2]].color;
-
 	draw_tri(screen_tri.pt1, screen_tri.pt2, screen_tri.pt3, data);
 }
 
